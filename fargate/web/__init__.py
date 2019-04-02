a90594a046bf8fb82617466758c275aa51440f39
thomasmburke <tburke@bu.edu>
2019-04-01 23:46:49 -0400

    call news api endpoint and push to web page

--- fargate/web/__init__.py
+++ fargate/web/__init__.py
@@ -1,6 +1,18 @@
-from flask import Flask
+import requests
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+API_KEY = '77e06fc676f04dcdbdf12b59a276f4f0'
+NEWS_API_URL = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={}'.format(API_KEY)
+
+def get_news_articles():
+    # fetch the news articles
+    response = requests.get(NEWS_API_URL)
+
+    # return the articles in json format
+    return response.json()['articles']
+
+@app.route("/")
+def index():
+    return str(get_news_articles())
