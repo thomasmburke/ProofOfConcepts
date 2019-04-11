f7a0d1df452ad70b8f95b5df2b5ea517ce4beb48
thomasmburke <tburke@bu.edu>
2019-04-11 11:36:58 -0400

    found most frequently occuring digits

--- pythonCollections/python_Counter.py
+++ pythonCollections/python_Counter.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import re
 
 # Find the top 5 most common letters in the samplestring
 sampleString = 'jhnruinflkwmviojenoignerongiurheonenkoneoinvon7483u85u23095io4ut483jiqu34nf984hfouncu9n834fh7nu432fjldknxcvbnmdfghjertyu'
@@ -6,3 +7,11 @@ c = Counter()
 for char in sampleString:
     c[char] += 1
 print(c.most_common(5))
+
+# Find top 5 most common numbers in sample string
+pattern = '\D'
+newString = re.sub(pattern, '', sampleString)
+digitCounter = Counter()
+for char in newString:
+    digitCounter[char] += 1
+print(digitCounter.most_common(5))
