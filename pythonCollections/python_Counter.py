f96380bf92c4cbb7eaa57f19ee964e7f673b0428
thomasmburke <tburke@bu.edu>
2019-04-11 11:50:18 -0400

    Counter arithmetic

--- pythonCollections/python_Counter.py
+++ pythonCollections/python_Counter.py
@@ -24,3 +24,22 @@ digitCounter.update(myList)
 
 mostCommonDigit = digitCounter.most_common(1)
 print(mostCommonDigit)
+
+# Counter Arithmetic
+c1 = Counter(['a', 'b', 'c', 'a', 'b', 'b'])
+c2 = Counter('alphabet')
+
+print('C1:', c1)
+print('C2:', c2)
+
+print('\nCombined counts:')
+print(c1 + c2)
+
+print('\nSubtraction:')
+print(c1 - c2)
+
+print('\nIntersection (taking positive minimums):')
+print(c1 & c2)
+
+print('\nUnion (taking maximums):')
+print(c1 | c2)
