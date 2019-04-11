78cd629b53c54e17a7404733b89f767ffb4cc72b
thomasmburke <tburke@bu.edu>
2019-04-11 11:43:04 -0400

    discovered the update function

--- pythonCollections/python_Counter.py
+++ pythonCollections/python_Counter.py
@@ -4,14 +4,18 @@ import re
 # Find the top 5 most common letters in the samplestring
 sampleString = 'jhnruinflkwmviojenoignerongiurheonenkoneoinvon7483u85u23095io4ut483jiqu34nf984hfouncu9n834fh7nu432fjldknxcvbnmdfghjertyu'
 c = Counter()
-for char in sampleString:
-    c[char] += 1
+#for char in sampleString:
+#    c[char] += 1
+c.update(sampleString)
 print(c.most_common(5))
 
 # Find top 5 most common numbers in sample string
 pattern = '\D'
 newString = re.sub(pattern, '', sampleString)
 digitCounter = Counter()
-for char in newString:
-    digitCounter[char] += 1
+#for char in newString:
+#    digitCounter[char] += 1
+digitCounter.update(newString)
 print(digitCounter.most_common(5))
+mostCommonDigit = digitCounter.most_common(1)
+print(mostCommonDigit)
