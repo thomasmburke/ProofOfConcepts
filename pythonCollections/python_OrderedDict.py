639f734ae4c699e542e729575a9f9190b59fd100
thomasmburke <tburke@bu.edu>
2019-04-11 16:03:01 -0400

    creating OrderedDicts ordered by some criteria

--- pythonCollections/python_OrderedDict.py
+++ pythonCollections/python_OrderedDict.py
@@ -13,3 +13,20 @@ print(d)
 # We can change popitem to FIFO if last is set to false
 print(d.popitem(last=False))
 print(d)
+
+d = {'banana': 3, 'apple': 4, 'pear': 1, 'orange': 2}
+# dictionary sorted by key
+d2 = OrderedDict(sorted(d.items(), key=lambda t: t[0]))
+
+# dictionary sorted by value
+d3 = OrderedDict(sorted(d.items(), key=lambda t: t[1]))
+
+# dictionary sorted by length of the key string
+d4 = OrderedDict(sorted(d.items(), key=lambda t: len(t[0])))
+print(d)
+print('sorted by key...')
+print(d2)
+print('sorted by value...')
+print(d3)
+print('sorted by key string length')
+print(d4)
